cc3b67ccc54359c242fc1826fdfaa07cd4ab2a4b
eminetekinm <eminetekin706@gmail.com>
2022-12-04 14:49:21 +0300

    Add project files.

---
+++ AriBilgi.Blog.UI/wwwroot/layout.js
@@ -0,0 +1,26 @@
+﻿var app = angular.module('WebsiteApp', []);
+
+app.controller("LayoutController", function ($scope, $http) {
+
+    angular.element(document).ready(function () {
+        $scope.GetCategoryList();
+       
+    });
+
+    $scope.GetCategoryList = function () {
+        $http({
+            method: "GET",
+            headers: { "Contet-Type": "Application/json;charset=utf-8" },
+            url: "https://localhost:44309/Api/Category/CategoryGetList",
+        }).then(function (d) {
+            $scope.CategoryList = d.data.data;
+        });
+    }
+    $scope.SupportOpen = function () {
+
+        var link = "https://api.whatsapp.com/send?phone=905516448080&text=Merhabalar";
+        window.open(link, "_blank");
+
+    }
+  
+});
